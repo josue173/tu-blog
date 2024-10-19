0f2c285f2f60719a224460c8d0c022a1bc38d02a
josue173 <josue.perez04@outlook.com>
2024-10-19 15:48:33 -0600

    Agregando roles y error de eslint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RolesModule } from './roles/roles.module';
+import { RolesController } from './roles/roles.controller';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [RolesModule],
+  controllers: [AppController, RolesController],
   providers: [AppService],
 })
 export class AppModule {}
