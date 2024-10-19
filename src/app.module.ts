01b96e9c9a3c25a3c4f79d7616a36a7724f90841
josue173 <josue.perez04@outlook.com>
2024-10-19 16:33:00 -0600

    docker-compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { RolesModule } from './roles/roles.module';
 import { RolesController } from './roles/roles.controller';
 import { RolesModule } from './roles/roles.module';
 
 @Module({
   imports: [RolesModule],
-  controllers: [AppController, RolesController],
-  providers: [AppService],
+  controllers: [RolesController],
+  providers: [],
 })
 export class AppModule {}
