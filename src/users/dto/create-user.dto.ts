f9fd8523e7f33264610eddf8c3ebc4ee06427c4e
josue173 <josue.perez04@outlook.com>
2024-11-19 20:05:11 -0600

    Create User

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { Type } from 'class-transformer';
+import { IsDate, IsEmail, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  username: string;
+
+  @IsString()
+  user_first_name: string;
+
+  @IsString()
+  user_last_name: string;
+
+  @IsEmail()
+  user_email: string;
+
+  @IsString()
+  user_password: string;
+
+  @IsDate()
+  @Type(() => Date)
+  user_birthday: Date;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  private readonly logger = new Logger('UsersService');
+
+  constructor(
+    @InjectRepository(User)
+    private readonly _userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const user = this._userRepository.create(createUserDto);
+      await this._userRepository.save(user);
+      return user;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
   findAll() {
@@ -22,5 +43,12 @@ export class UsersService {
 
   remove(id: number) {
     return `This action removes a #${id} user`;
+  }
+
+  private handleExceptions(error: any) {
+    if (error.code === 'ER_DUP_ENTRY')
+      throw new BadRequestException(error.sqlMessage);
+    this.logger.error(error);
+    throw new InternalServerErrorException('Unexpected error');
   }
 }
