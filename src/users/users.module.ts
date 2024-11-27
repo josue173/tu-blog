d1450688ec48ecb9dd507a70d58f1b384173f4f0
josue173 <josue.perez04@outlook.com>
2024-11-26 19:52:24 -0600

    Iniciando Autenticacion

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,36 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [UsersController],
   exports: [UsersService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+
+    JwtModule.registerAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => {
+        return {
+          secret: process.env.JWT_SECRET,
+          signOptions: {
+            expiresIn: '12h',
+          },
+        };
+      },
+    }),
+
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: {
+    //     expiresIn: '12h',
+    //   },
+    // }),
+  ],
   providers: [UsersService],
 })
 export class UsersModule {}
